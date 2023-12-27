5ffdba209dbe4a25004448a3ca8ba4830496d775
Arcanjo <136854236+ArcanjoSUP@users.noreply.github.com>
2023-12-27 18:56:53 -0300

    desafio-felipao

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 //Glória a Deus e a Jesus Cristo
 console.log("oi")
 //oi
+
+let nomeDoHeroi = "Arcanjo";
+let xpDoHeroi = 5000; 
+
+
+let nivelDoHeroi;
+
+if (xpDoHeroi < 1000) {
+  nivelDoHeroi = "Ferro";
+} else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+  nivelDoHeroi = "Bronze";
+} else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
+  nivelDoHeroi = "Prata";
+} else if (xpDoHeroi >= 5001 && xpDoHeroi <= 7000) {
+  nivelDoHeroi = "Ouro";
+} else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000) {
+  nivelDoHeroi = "Platina";
+} else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
+  nivelDoHeroi = "Ascendente";
+} else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000) {
+  nivelDoHeroi = "Imortal";
+} else {
+  nivelDoHeroi = "Radiante";
+}
+
+
+console.log(`${nomeDoHeroi} atingiu o nível ${nivelDoHeroi} com ${xpDoHeroi} XP.`);
